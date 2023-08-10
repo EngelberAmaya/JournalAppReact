2a400a4eaf60e48351d99b848c00c4e527631242
EngelberAmaya <engelberamaya@gmail.com>
2023-08-10 18:35:48 -0400

    Restructuring

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,26 +1,184 @@
-import { Box, Toolbar } from "@mui/material"
-import { NavBar, SideBar } from "../components"
+// import { Box, Toolbar } from "@mui/material"
+// import { NavBar, SideBar } from "../components"
 
-const drawerWidth = 280;
+import { Box, Container, CssBaseline, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import { useState } from "react";
+import { styled } from '@mui/material/styles';
+import MuiAppBar from '@mui/material/AppBar';
+import MuiDrawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+
+import { LogoutOutlined, TurnedInNot } from "@mui/icons-material";
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import Grid from '@mui/material/Grid';
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      '& .MuiDrawer-paper': {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        boxSizing: 'border-box',
+        ...(!open && {
+          overflowX: 'hidden',
+          transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+          }),
+          width: theme.spacing(7),
+          [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+          },
+        }),
+      },
+    }),
+  );
+
+// const drawerWidth = 280;
 
 // eslint-disable-next-line react/prop-types
 export const JournalLayout = ({ children }) => {
 
+    // <Box sx={{ display: 'flex'}}>
+
+    //     <NavBar drawerWidth = {drawerWidth} />
+
+    //     <SideBar drawerWidth = {drawerWidth} />
+
+    //     <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+
+    //         <Toolbar />
+
+    //         { children }
+
+    //     </Box>
+    
+    // </Box>
+    
+    const [open, setOpen] = useState(true);
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
     return (
-        <Box sx={{ display: 'flex'}}>
 
-            <NavBar drawerWidth = {drawerWidth} />
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="absolute" open={open}>
+                <Toolbar  sx={{ pr: '24px' }}>
+                    
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={toggleDrawer}
+                        sx={{
+                            marginRight: '36px',
+                            ...(open && { display: 'none' }),
+                        }}>
+                             <MenuIcon />
+                    </IconButton>
 
-            <SideBar drawerWidth = {drawerWidth} />
+                    <Typography
+                        component="h1"
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        sx={{ flexGrow: 1 }}
+                    >
+                        JournalApp
+                    </Typography>
 
-            <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+                    <IconButton color="error">
+                        <LogoutOutlined />
+                    </IconButton>
 
-                <Toolbar />
+                </Toolbar>
+            </AppBar>
 
-                { children }
+            <Drawer variant="permanent" open={open}>
+                <Toolbar
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'flex-end',
+                        px: [1],
+                    }}
+                >
+                    <IconButton onClick={toggleDrawer}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </Toolbar>
+                <Divider />
 
+                <List>
+                    {
+                        ['Enero','Febrero','Marzo','Abril'].map( text => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <TurnedInNot />
+                                    </ListItemIcon>
+
+                                    <Grid container>
+                                        <ListItemText primary={text} />
+                                        <ListItemText secondary={' texto'}/>
+                                    </Grid>
+
+                                </ListItemButton>
+                            </ListItem>
+                        ))
+                    }
+                </List>
+            </Drawer>
+
+            <Box
+                component="main"
+                sx={{
+                    backgroundColor: (theme) =>
+                    theme.palette.mode === 'light'
+                        ? theme.palette.grey[100]
+                        : theme.palette.grey[900],
+                    flexGrow: 1,
+                    height: '100vh',
+                    overflow: 'auto',
+                }}
+            >
+                 <Toolbar />
+                 <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+
+                    { children }
+
+                 </Container>
             </Box>
-        
+
         </Box>
     )
 }
