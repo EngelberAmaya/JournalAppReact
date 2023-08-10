5423a4725d4e79a79b24ea3d142359175ac6665c
EngelberAmaya <engelberamaya@gmail.com>
2023-08-10 14:51:01 -0400

    NavBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,12 +1,15 @@
 import { Box } from "@mui/material"
+import { NavBar } from "../components"
 
-//const drawerWidth = 240;
+const drawerWidth = 240;
 
 // eslint-disable-next-line react/prop-types
 export const JournalLayout = ({ children }) => {
 
     return (
         <Box sx={{ display: 'flex'}}>
+
+            <NavBar drawerWidth = {drawerWidth} />
 
             <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
 
